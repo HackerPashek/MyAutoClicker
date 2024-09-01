afbd85fd6c61397701ea287a1d37a345516f2a4c
HackerPashek <g907554@gmail.com>
2024-09-01 10:07:27 +0300

    Create AutoClicker.py

---
+++ AutoClicker.py
@@ -0,0 +1,29 @@
+import sys
+import keyboard
+import pyautogui
+import time
+
+is_Clicking = False
+
+def Click():
+	global is_Clicking
+	if not is_Clicking:
+		is_Clicking = True
+		print("On")
+	else:
+		is_Clicking = False
+		print("Off")
+
+def Quit():
+	global is_Clicking
+	is_Clicking = False
+	print("Quit")
+	sys.exit()
+
+keyboard.add_hotkey("x", Click)
+keyboard.add_hotkey("q", Quit)
+
+while True:
+	if is_Clicking:
+		pyautogui.click(button = "left") 
+		time.sleep(0.01)
